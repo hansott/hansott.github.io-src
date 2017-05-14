e46f2d39b95bbbc119d6af9f148f3f4b8bc4f05e
Hans Ott <hansott@hotmail.be>
2017-05-14 12:00:06 +0200

    ✨ Show anchor like GitHub's markdown

--- themes/hansott/static/js/main.js
+++ themes/hansott/static/js/main.js
@@ -47,9 +47,52 @@
         }
     }
 
+    function handleLinkAnchorClick(event) {
+        var heading = event.currentTarget.parentNode;
+        window.location.hash = heading.id;
+    }
+
+    function handleLinkMouseEnter(event) {
+        var heading = event.currentTarget;
+        var anchor = heading.querySelector('.anchor');
+        if (anchor) {
+            return;
+        }
+        var link = document.createElement('a');
+        link.classList.add('anchor');
+        var icon = document.createElement('i');
+        icon.classList.add('fa');
+        icon.classList.add('fa-link');
+        link.appendChild(icon);
+        heading.appendChild(link);
+        link.addEventListener('click', handleLinkAnchorClick);
+    }
+
+    function handleLinkMouseLeave(event) {
+        var heading = event.currentTarget;
+        var anchor = heading.querySelector('.anchor');
+        if (anchor) {
+            heading.removeChild(anchor);
+        }
+    }
+
+    function initLinkAnchor() {
+        var contents = document.getElementsByClassName('content');
+        for (var i = 0; i < contents.length; i++) {
+            var content = contents[i];
+            var headings = content.querySelectorAll('h1[id], h2[id], h3[id], h4[id], h5[id], h6[id]');
+            for (var h = 0; h < headings.length; h++) {
+                var heading = headings[h];
+                heading.addEventListener('mouseenter', handleLinkMouseEnter);
+                heading.addEventListener('mouseleave', handleLinkMouseLeave);
+            }
+        }
+    }
+
     function init() {
         initMobileMenu();
         initShareButtons();
+        initLinkAnchor();
     }
 
     document.addEventListener('DOMContentLoaded', init);
